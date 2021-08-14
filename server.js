69d016115a788989929f02089d46b720505bb68b
Jose Alexandre <allexandregmai@gmail.com>
2021-08-14 13:06:58 -0400

    Testando o commit

--- server.js
+++ server.js
@@ -12,16 +12,16 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // conexão com banco de dados e configurações para não ocorrer erro
-mongoose.connect('mongodb://localhost:27017/db-admin-ifro',{
-  useUnifiedTopology: true,
-  useNewUrlParser: true,
-  useFindAndModify: false
+mongoose.connect('mongodb://localhost:27017/db-admin-ifro', {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useFindAndModify: false
 }, function(err) {
-  if (err) {
-    console.log('Falha na conexão com MongoDB: '+err)
-  } else {
-    console.log('MongoDB conectado com sucesso! Porta: '+port)
-  }
+    if (err) {
+        console.log('Falha na conexão com MongoDB: ' + err)
+    } else {
+        console.log('MongoDB conectado com sucesso! Porta: ' + port)
+    }
 })
 
 app.use(cors());
@@ -30,5 +30,7 @@ app.use(express.json());
 app.use(routes);
 
 app.listen(port, function() {
-  console.log('Server is running!')
+    console.log('Server is running!')
 })
+
+console.log("Teste")
