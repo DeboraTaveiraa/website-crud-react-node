a4246b53fb0ce9295401e042975e14a82f24aa9c
Débora Taveira <deborataveirapvh@gmail.com>
2021-08-18 22:15:02 -0400

    criado função e rotas para listagem dos usuarios do mongodb

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,11 @@ const routes = express.Router();
 const Usuario = require('./controllers/usuarios.controller');
 
 routes.get('/', Usuario.index);
+
+// ROTAS DE USUÁRIOS
 routes.post('/api/usuarios' ,Usuario.create);
+routes.get('/api/usuarios', Usuario.index);
+routes.get('/api/usuarios-details/:_id', Usuario.details);
 
 // exportando funções de rotas
 module.exports = routes;
