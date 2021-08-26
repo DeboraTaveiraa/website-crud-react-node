a7fa159560d2822bb6fe37d3c1ea0fd2b1521855
Débora Taveira <deborataveirapvh@gmail.com>
2021-08-26 00:10:54 -0400

    Criado Dashboard e Menu Admin

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Inicio from './pages/client/painel/index';
 import Routes from './routes';
 
 function App() {
---
+++ client/src/components/footer-admin.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+
+export default function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}      
+        Sistema Adiministrativo  {' '}     
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
